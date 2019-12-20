57dadfb922eed947a3694b31b1b68f8d8fdc7b3d
Mike Singleton <mike@pickaxe.nyc>
2019-12-20 17:50:17 -0500

    Init workspace

---
+++ Iterate/Iterate.h
@@ -0,0 +1,19 @@
+//
+//  Iterate.h
+//  Iterate
+//
+//  Created by Michael Singleton on 12/20/19.
+//  Copyright © 2019 Iterate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Iterate.
+FOUNDATION_EXPORT double IterateVersionNumber;
+
+//! Project version string for Iterate.
+FOUNDATION_EXPORT const unsigned char IterateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Iterate/PublicHeader.h>
+
+
